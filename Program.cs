a141e540e4a66b8b63002b02c090c7890dbaba14
KrisitnaS <114879169+KrisitnaS@users.noreply.github.com>
2022-10-17 01:07:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,51 @@
 ﻿System.Console.WriteLine();
 
-System.Console.Write("Введите число:   ");
-string writeN = Console.ReadLine();
-int N = Convert.ToInt32(writeN);
+int N = new Random().Next(1, 30);
+System.Console.WriteLine($"N = {N}");
 
-int GetNumbersComposition(int x)
+// решение через цикл 
+
+int number = 1;
+System.Console.WriteLine("By While");
+while (number <= N)
 {
-    int comp = 1;
-    for (int num = 1; num <= x; num++)
+    int cube = number * number * number;
+    if (cube % 2 == 0) System.Console.Write(cube + " ");
+    number++;
+}
+System.Console.WriteLine();
+
+// решение через метод
+
+System.Console.WriteLine("By Method 1:");
+
+int[] GetEvenCubeArray(int N)
+{
+    int num = 1;
+    int i = 0;
+    int[] EvenCubs = new int[N];
+    while (num <= N)
     {
-        comp = comp * num;
+        int cube = num * num * num;
+        if (cube % 2 == 0)
+        {
+            EvenCubs[i] = cube;
+            i++;
+        }
+        num++;
     }
-    return comp;
+    return EvenCubs;
 }
 
-System.Console.WriteLine($"Произведение чисел от 1 до {N} равно {GetNumbersComposition(N)}");
-System.Console.WriteLine();
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+PrintArray(GetEvenCubeArray(N));
+
+
