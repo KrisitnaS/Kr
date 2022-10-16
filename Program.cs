024b959b10b75cc5f8cf27820e42e3b8405efcb2
KrisitnaS <114879169+KrisitnaS@users.noreply.github.com>
2022-10-16 20:38:56 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int numberA=4;
+int numberB=16;
+if (numberB ==(numberA*numberA))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("think more,");
+}
