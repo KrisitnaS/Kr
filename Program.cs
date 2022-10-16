ba1b8118784113c3e7015ebb64751aaaa0463d07
KrisitnaS <114879169+KrisitnaS@users.noreply.github.com>
2022-10-16 23:16:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+System.Console.Write("Введите число:   ");
+string writeNumber = Console.ReadLine();
+int endNumber = Convert.ToInt32(writeNumber);
+int Number = endNumber * (-1);
+System.Console.Write($"{Number} ");
+
+while (Number < endNumber)
+{
+    Number++;
+    System.Console.Write($"{Number} ");
+}
+System.Console.WriteLine(); System.Console.WriteLine();
+
